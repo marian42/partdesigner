20cdb25d1ab016f0ab4e5af46ea1f0986a96cbbc
Marian Kleineberg <mail@marian42.de>
2019-03-02 18:47:59 +0100

    Initial commit

---
+++ app.ts
@@ -0,0 +1,28 @@
+﻿class MeshRenderer {
+    canvas: HTMLCanvasElement;
+	gl: WebGLRenderingContext;
+
+
+    constructor(canvas: HTMLCanvasElement) {
+        this.canvas = canvas;
+		this.gl = canvas.getContext("webgl") as WebGLRenderingContext;
+
+		if (this.gl == null) {
+			throw new Error("WebGL is not supported.");
+		}
+    }
+
+	update() {
+		this.gl.clearColor(0.0, 0.0, 0.0, 1.0);
+		this.gl.clear(this.gl.COLOR_BUFFER_BIT);
+	}
+}
+
+window.onload = () => {
+	var part = new Part();
+	part.randomize();
+
+    var el = document.getElementById('canvas') as HTMLCanvasElement;
+    var renderer = new MeshRenderer(el);
+    renderer.update();
+};
